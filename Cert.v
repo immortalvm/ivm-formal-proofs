d988abcab263b6b01c1738c6b4b4d9d94c60ce00
Ivar Rummelhoff <ivarrru@gmail.com>
2021-03-29 16:31:47 +0200

    Unstaged changes January 2

--- Cert.v
+++ Cert.v
@@ -9,7 +9,191 @@ Import OpCodes.
 Local Notation not_terminated := (ret true) (only parsing).
 Local Notation terminated := (ret false) (only parsing).
 
-(******************************)
+(***********)
+
+
+
+  induction n.
+  - simp popMany. rewrite ret_bind.
+    cbn. rewrite pushMany_empty.
+    apply (smonad_ext' SP). intros a. rewrite lens_put_get.
+    apply bind_extensional. intros [].
+    destruct (decide (nAvailable 0 a)) as [H|H].
+    + reflexivity.
+    + contradict H. constructor.
+  - simp popMany.
+    repeat setoid_rewrite bind_assoc.
+    setoid_rewrite ret_bind.
+    setoid_rewrite to_list_equation_2.
+    assert (forall X (x:X) (u:list X), x :: u = [x] ++ u)%list as Hl.
+    {
+      intros. reflexivity.
+    }
+    setoid_rewrite Hl.
+    setoid_rewrite pushMany_action.
+    setoid_rewrite <- bind_assoc at 1.
+    setoid_rewrite IHn.
+    setoid_rewrite bind_assoc.
+    setoid_rewrite pushMany_one.
+
+    repeat setoid_rewrite postpone_assume''.
+    rewrite pop_getSP.
+    setoid_rewrite <- bind_assoc at 1.
+    setoid_rewrite pop_push.
+    setoid_rewrite bind_assoc.
+    setoid_rewrite lens_get_get.
+
+
+    rewrite pop_spec, push_spec.
+
+    repeat setoid_rewrite bind_assoc.
+
+
+
+
+
+
+Admitted.
+
+(***)
+
+Proposition popN_pushN n :
+  let* u := popN n in
+  pushN u ⊑ ret tt.
+Proof.
+  rewrite popN_spec, pushN_spec.
+  setoid_rewrite bind_assoc.
+  setoid_rewrite ret_bind.
+  setoid_rewrite longsToBytes_bytesToLongs.
+
+  rewrite popMany_spec.
+  unfold pushMany.
+  rewrite pushMany_spec.
+
+(***)
+
+Lemma popN_e n {X} {RX: Rel X} {TX: Transitive RX}
+    (f: vector B64 n -> M X) (mx: M X)
+    (H: forall (u: vector B64 n), f u ⊑ pushN u;; mx) :
+  let* u := popN n in
+  f u ⊑ mx.
+Proof.
+  assert (
+    let* u := popN n in f u ⊑ let* u := popN n in pushN u;; mx
+  ) as HH.
+  {
+    apply bind_propr'.
+    - rewrite popN_spec. crush.
+    - intros u u' Hu. destruct Hu. apply H.
+  }
+  rewrite <- bind_assoc in HH.
+
+
+(******)
+
+Lemma stdStart_lemma m n {o} (ops: vector Z o) :
+  stdStart m n ops =
+    let* sp := get' SP in
+    let* pc := get' PC in
+    let* inpBytes := loadMany (n * 8) sp in
+    let* actualOps := loadMany o pc in
+    assume' (nBefore (m * 8) sp # nAfter o pc);;
+    assume' (nAfter (n * 8) sp # nAfter o pc);;
+    assume' (actualOps = Vector.map toB8 ops);;
+    wipe (nBefore (m * 8) sp);;
+    wipe (nAfter (n * 8) sp);;
+    put' SP (offset (n * 8) sp);;
+    put' PC (offset o pc);;
+    ret (bytesToLongs inpBytes).
+Proof.
+  unfold stdStart.
+  rewrite popN_spec, popMany_spec, swallow_spec.
+  unfold wipeStack, wipe'.
+
+  repeat setoid_rewrite bind_assoc.
+  setoid_rewrite ret_bind.
+  setoid_rewrite <- confined_put.
+
+  smon_rewrite.
+
+
+Definition stdDis m n o :=
+  let* sp := get' SP in
+  let* pc := get' PC in
+  assume (nBefore (m * 8) sp # nAfter o pc);;
+  assume (nAfter (n * 8) sp # nAfter o pc);;
+  ret tt.
+
+
+
+Lemma wipeStack_swallow n ops :
+  wipeStack n
+
+
+
+
+
+(***)
+
+Proposition cert_PUSH0_NOT :
+  nCertN 2 (
+    wipeStack 1;;
+    swallow [PUSH0; NOT];;
+    pushZ (-1)
+  ).
+Proof.
+  unfold nCertN, nCert, wipeStack.
+  smon_rewrite.
+  apply (rel_extensional' SP). intros a a' Ha. destruct Ha.
+  rewrite lens_put_get.
+  unfold wipe.
+  setoid_rewrite confined_wipe.
+
+idtac.
+
+
+
+
+cert_PUSH0
+
+
+
+
+
+
+
+
+
+
+
+
+(* TODO: Useful? *)
+
+Proposition loadMany_rel
+    n a f {X} mx
+    (H: forall (Ha: nAvailable n a) u, f u ⊑ storeMany a u;; mx) :
+  let* u := loadMany n a in
+  f u ⊑ mx.
+loadMany
+
+
+Proposition popMany_rel n :
+
+popMany
+loadMany
+
+Lemma popN_rel
+
+
+Lemma itest
+    m n {o} (ops: vector Z o)
+    {X} (f: vector B64 n -> M X) (mx: M X)
+    (H: forall (Ha: ))
+    :
+  let* v := stdStart m n ops in
+  f v ⊑ mx.
+Proof.
+
 
 (******************************)
 
@@ -150,34 +334,121 @@ Proof.
 
 (*************************************************)
 
+(******************)
+
+(** For convenience *)
+Proposition puge {A} {LA: Lens State A} s {X} (f: A -> M X) :
+  put s;;
+  let* a := get' LA in
+  f a =
+    put s;;
+    f (proj LA s).
+Proof.
+  rewrite get_spec.
+  smon_rewrite.
+Qed.
+
+Instance put_s_propr (s: State) : PropR (put s).
+Proof.
+  crush.
+Qed.
+
+Proposition wipe_wiped u :
+  wipe' u ⊑ wiped u.
+Proof.
+  unfold wipe', wipe, wiped, isWiped.
+  rel_extensional_1.
+  {
+    crush.
+    apply putMem'_propr.
+    intros _ _ _.
+    crush.
+  }
+  intros s.
+  rewrite <- postpone_assume'.
+  rewrite put_spec. cbn.
+  rewrite put_get'.
+  unfold compose.
+  setoid_rewrite put_put.
+  rewrite puge.
+  crush.
+  assert (forall a : Addr, a ∈ u ->
+    exists Ha : available a, proj MEM s a Ha = None) as HP.
+  {
+    intros a Hu.
+    exists (HL a Hu).
+
+  }
 
 
-Definition stdDis m n o :=
-  let* sp := get' SP in
-  let* pc := get' PC in
-  assume (nBefore (m * 8) sp # nAfter o pc);;
-  assume (nAfter (n * 8) sp # nAfter o pc);;
-  ret tt.
 
+
+(****************)
+
+(* TODO: Define nAvailable similarly?
+nAvailable u := nAfter n a ⊆ available *)
+
+Proposition wiped_assumeState u :
+  wiped u = assumeState (fun s => isWiped u (proj MEM s)).
+Proof.
+  unfold wiped. rewrite get_spec. smon_rewrite.
+Qed.
+
+Proposition wipe_assume n :
+  wipeStack n ⊑
+    let* sp := get' SP in
+    wiped (nBefore (n * 8) sp).
+Proof.
+  unfold wipeStack, wipe, wiped, isWiped.
+
+
+
+
+Lemma wipeStack_isWiped n
+    {X} (mx mx': M X)
+    (H: is wiped ) :
+  wipeStack n;;
+  mx ⊑ mx'.
+
+
+
+(***)
+
+(* TODO: Useful? *)
+Proposition rel_ret_tt
+            mu Y (my my' : M Y)
+            `(mu ⊑ ret tt)
+            `(my ⊑ my') : mu;; my ⊑ my'.
+Proof.
+  assert (my' = ret tt;; my') as HH.
+  - rewrite ret_bind. reflexivity.
+  - rewrite HH. crush; assumption.
+Qed.
+
+(* TODO: Postpone? *)
+Definition w_pop64 := let* v := pop64 in
+                      wipeStack 1;;
+                      ret v.
+
+Corollary wiped_pop64 : w_pop64 ⊑ pop64.
+Proof.
+  unfold w_pop64.
+  rewrite <- bind_ret.
+  crush.
+  apply rel_ret_tt.
+  - apply wipeStack_less.
+  - crush.
+Qed.
 
 
 (* TODO: Move *)
 
 (** In VectorSpec.v: [shiftin_last] *)
 
-
-
 (* ----------------------------------- *)
-
-
-
-
-(* ----------------------------------- *)
-
 
     setoid_rewrite nAfter_empty.
     unfold nAfter.
-
 
 setoid_rewrite nAfter_spec.
 
@@ -187,8 +458,6 @@ setoid_rewrite nAfter_spec.
     apply not_nAfter_disjoint_evidence in H.
   destruct H as [x [Hx [i [Hi Ho]]]];
     subst x.
-
-
 
 Proposition stdStart_stdDis m n {o} (ops: vector Z o) :
   stdDis m n o;; stdStart m n ops = stdStart m n ops.
@@ -241,8 +510,6 @@ Proof.
 
     setoid_rewrite get_spec.
     setoid_rewrite get_spec.
-
-
 
 unfold wipe.
 
